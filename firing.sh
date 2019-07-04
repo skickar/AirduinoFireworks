1df10399144c20d1425a49c7a9162c1ac5e246d9
Skickar <40251293+skickar@users.noreply.github.com>
2019-07-03 19:25:24 -0700

    Now she talks
    Made the computer talk while firing

--- firing.sh
+++ firing.sh
@@ -1,3 +1,3 @@
 #! bin/bash
 
-sleep 5 && curl http://192.168.4.1/analog/1/1 && sleep 5 && curl http://192.168.4.1/digital/1/1 && sleep 5 && curl http://192.168.4.1/digital/1/0
+sleep 5 && say "the computer has control, counting down from 5" && curl http://192.168.4.1/analog/1/1 && sleep 5 && say "firing" && curl http://192.168.4.1/digital/1/1 && sleep 5 && say "Safe" && curl http://192.168.4.1/digital/1/0
